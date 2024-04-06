076642f3042dbdc081de4fe21d35f0e4575fc218
Ben Brooks <ben9brooks@gmail.com>
2024-04-06 12:22:39 -0500

    upload button + installs

--- deepbreak/src/App.js
+++ deepbreak/src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import { UploadButton } from "@bytescale/upload-widget-react";
+
+const options = {
+  apiKey: "public_12a1yuP2GtyBHjybdh9CyYkHFP6k", // This is your API key.
+  maxFileCount: 1
+};
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          DEEPBREAK
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <div className='App-body'>
+        <UploadButton options={options}
+                      onComplete={files => alert(files.map(x => x.fileUrl).join("\n"))}>
+          {({onClick}) =>
+            <button onClick={onClick}>
+              Upload a file...
+            </button>
+          }
+        </UploadButton>
+        <img src="https://i.pinimg.com/originals/e0/50/29/e05029add9bfd9db2db88264b375257a.jpg"/>
+      </div>
     </div>
   );
 }
