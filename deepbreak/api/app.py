bc383c129d65ac32225f8494df36d52b013a93db
Sudeep Potluri <spotluri20@gmail.com>
2024-04-07 07:57:41 -0500

    CLeaned up code and removed blur

--- deepbreak/api/app.py
+++ deepbreak/api/app.py
@@ -20,25 +20,11 @@ processed_image = [{}]
 def hello_world():
     return 'Hello, World!'
 
-# @app.route('/upload', methods=['POST'])
-# def deepbreak():
-#     imageb64 = (request.get_json())['image']
-#     prefix = 'data:image/webp;base64,'
-#     cut = imageb64[len(prefix):]
-#     im = Image.open(io.BytesIO(base64.b64decode(cut)))
-#     np_image = np.array(im)
-#     identifyFaces(np_image)
-#     new_image = process_image(np_image)
-#     converted_string = base64.b64encode(new_image) 
-#     returnValue = [{'image': f"{converted_string}"}]
-#     return jsonify(data=returnValue, status=200, mimetype='application/json')
-
 @app.route('/upload', methods=['POST'])
 def deepbreak():
     imageb64 = (request.get_json())['image']
     imageb64 = imageb64.split(',')
     imageb64 = imageb64[1]
-    # print(len(imageb64))
     im = Image.open(io.BytesIO(base64.b64decode(imageb64)))
     np_image = np.array(im)
     detected_faces = identifyFaces(np_image)
@@ -53,23 +39,6 @@ def deepbreak():
 def invert_pixel_color(pixel):
     # Assuming the pixel is in RGB format
     return  50 - pixel
-
-# def identifyFaces(image):
-#     gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-#     face_classifier = cv2.CascadeClassifier(
-#     cv2.data.haarcascades + "haarcascade_frontalface_default.xml"
-#     )
-#     face = face_classifier.detectMultiScale(
-#     gray_image, scaleFactor=1.1, minNeighbors=5, minSize=(40, 40)
-#     )
-#     for (x, y, w, h) in face:
-#         cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 4)
-#     img_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    
-
-#     img = Image.fromarray(img_rgb, 'RGB')
-#     img.save('my.png')
-#     img.show()
 
 def identifyFaces(image):
     gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
@@ -95,37 +64,17 @@ def process_image(image, faces):
                     processed_image[i, j] = invert_pixel_color(processed_image[i, j])
         face_region = processed_image[y:y+h, x:x+w]
         
+        #Was messing with blurring images. Makes the generated images work better so stopped this. 
         # Generate uniform noise with reduced intensity
-        noise = np.random.randint(-10, 10, size=face_region.shape, dtype=np.int8)  # Uniform noise
-        noisy_face_region = np.clip(face_region + noise, 0, 255)  # Ensure pixel values are within [0, 255] range
+        # noise = np.random.randint(-10, 10, size=face_region.shape, dtype=np.int8)  # Uniform noise
+        # noisy_face_region = np.clip(face_region + noise, 0, 255)  # Ensure pixel values are within [0, 255] range
         
-        # Apply blurring to reduce noise
-        blurred_face_region = cv2.GaussianBlur(noisy_face_region, (5, 5), 0)  # Gaussian blur with a 5x5 kernel size
+        # # Apply blurring to reduce noise
+        # blurred_face_region = cv2.GaussianBlur(noisy_face_region, (5, 5), 0)  # Gaussian blur with a 5x5 kernel size
         
-        # Update the processed image with the blurred face region
-        processed_image[y:y+h, x:x+w] = blurred_face_region
+        # # Update the processed image with the blurred face region
+        # processed_image[y:y+h, x:x+w] = blurred_face_region
     return processed_image
 
-# def process_image(image):
-#     """
-#     Takes an image represented as a 3D NumPy array (height x width x channels)
-#     and inverts one pixel's color out of every group of 10 pixels in each row.
-#     """
-#     # Copy the image to avoid altering the original
-#     processed_image = np.copy(image)
-    
-#     height, width, _ = image.shape
-    
-#     # Iterate over each row
-#     for i in range(height):
-#         # Iterate over each group of 10 pixels in the row
-#         for j in range(0, width, 10):
-#             # Choose which pixel to invert in this group
-#             pixel_to_invert = j  # For simplicity, always choose the first pixel in the group
-#             if pixel_to_invert < width:  # Check to avoid index error at the end of a row
-#                 # Invert the color of the chosen pixel
-#                 processed_image[i, pixel_to_invert] = invert_pixel_color(processed_image[i, pixel_to_invert])
-    
-    return processed_image
 if __name__ == '__main__':
     app.run()
